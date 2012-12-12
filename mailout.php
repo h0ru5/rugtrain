6f090fa05057f513ce81b94c09017bb7154baa39
h0ru5 <johannes.hund@gmail.com>
2012-12-12 22:57:43 +0100

    some touch-up

--- mailout.php
+++ mailout.php
@@ -12,51 +12,64 @@ require_once("const.inc.php");
       fputs($fp,"> $row->tid: $row->what in $row->where in $row->diff days\n");
       if($row->diff == $daysahead) {
           fputs($fp,"mailing out...\n");
-          mailOutFor($row->tid,$fp);
+          mailOutFor($row,$fp);
       }
     }
     fputs($fp,"done!\n");
 
     fclose($fp);
 
-function mailOutFor($tid,$log) {
+function mailOutFor($evt,$log) {
     $data = & doQuery("SELECT name,email FROM mailliste ORDER BY name");
     fputs($log,"start mailing\n");
     while($row = $data->fetchRow()) {
         fputs($log,$row->name . ": " . $row->email . "\n");
-    #	mailout($row->name,$row->email,$tid);
+    #	mailout($row->name,$row->email,$evt);
     }
-    mailout("Johannes","rugby@johanneshund.de",$tid);
+    mailout("Johannes","rugby@johanneshund.de",$evt);
     fputs($log,"mailing done\n");
 }
 
-function mailout($name_i,$mailadd,$tid) {
+function mailout($name_i,$mailadd,$evt) {
 $name = urlencode($name_i);
-$td = trainday3($tid);
+$td = trainday3($evt->tid);
 
 $html_body = "
 <html>
 <head>
+<style type='text/css'>
+.logo, .logo * {
+    text-align: center;
+    display: block;
+    margin-left: auto;
+    margin-right: auto
+}
+</style>
 </head>
 
 <body>
-<h1>RugBears Training am $td</h1>
-<p>Kommst du? einfach den Link anklicken<br>Ergebnisse sind auf <a href='http://training.munichrugbears.de/$tid'>training.munichrugbears.de</a></p>
+<div class='logo'>
+<img src='http://training.munich-rugbears.de/img/logo_rb.jpg' alt='Rugbears logo' />
+<h1>RugBears $evt->what am $td</h1>
+<p>Ort: $evt->where</p>
+</div>
+
+<p>Kommst du? einfach den Link anklicken<br>Ergebnisse sind auf <a href='http://training.munichrugbears.de/$evt->tid'>training.munichrugbears.de</a></p>
 <table width='80%' style='table-layout:fixed'>
 <tr>
-<td width='33%'><a href='http://training.munich-rugbears.de/$tid/add?vote=1&name=$name'>Spiele</a></td>
-<td width='33%'><a href='http://training.munich-rugbears.de/$tid/add?vote=2&name=$name'>Komme</a></td>
-<td width='33%'><a href='http://training.munich-rugbears.de/$tid/add?vote=3&name=$name'>Komme nicht</a></td>
+<td width='33%'><a href='http://training.munich-rugbears.de/$evt->tid/add?vote=1&name=$name'>Spiele</a></td>
+<td width='33%'><a href='http://training.munich-rugbears.de/$evt->tid/add?vote=2&name=$name'>Komme</a></td>
+<td width='33%'><a href='http://training.munich-rugbears.de/$evt->tid/add?vote=3&name=$name'>Komme nicht</a></td>
 </tr>
 </table>
 </body>
 </html>";
 
 $text_body = "
-Rugbears Training am $td\n\nKommst du? einfach link anklicken\n
-Spiele: http://training.munich-rugbears.de/$tid/add?vote=3&name=$name\n\n
-Komme: http://training.munich-rugbears.de/$tid/add?vote=3&name=$name\n\n
-Komme nicht: http://training.munich-rugbears.de/$tid/add?vote=3&name=$name\n\n";
+Rugbears $evt->what am $td\nin $evt->where\n\nKommst du? einfach link anklicken\n
+Spiele: http://training.munich-rugbears.de/$evt->tid/add?vote=3&name=$name\n\n
+Komme: http://training.munich-rugbears.de/$evt->tid/add?vote=3&name=$name\n\n
+Komme nicht: http://training.munich-rugbears.de/$evt->tid/add?vote=3&name=$name\n\n";
 $grenze="Nextpart";
 
 $headers ="MIME-Version: 1.0\n";
