ee9aed92d55d1e9c72922d3f2cc48d2dfea45cc1
h0ru5 <johannes.hund@gmail.com>
2013-04-21 18:48:10 +0200

    added partresponses with fake email Signed-off-by:h0ru5 <johannes.hund@gmail.com>

--- res/calender.php
+++ res/calender.php
@@ -2,7 +2,14 @@
 require_once("phpicalwriter/iCalWriter.php");
 require_once("Trainings.php");
 
+//to be upstreamed into phpicalwriter
+$pstate[1]="ACCEPTED";
+$pstate[2]="TENTATIVE";
+$pstate[3]="DECLINED";
+
 function addEvents($cal) {
+
+    global $pstate;
     $all = allFutures()->fetchAll();
     foreach ($all as $tid) {
         $item = new iCalEvent;
@@ -12,21 +19,23 @@ function addEvents($cal) {
         $item->setLocation($it->where);
         $item->setShortDescription($it->what);
         $dat = new DateTime($it->when);
-        $item->setStart($dat->format("Y"),$dat->format("m"),$dat->format("d"), 0, 1, "", 1);
+        $item->setStart($dat->format("Y"),$dat->format("m"),$dat->format("d"), 0, 1, "", 1,"00","00");
         $dat->modify("+1 day"); //refractor dates to start and end
-        $item->setEnd($dat->format("Y"),$dat->format("m"),$dat->format("d"),0, 1, "", 1);
+        $item->setEnd($dat->format("Y"),$dat->format("m"),$dat->format("d"),0, 1, "", 1,"00","00");
         $item->setDuration(0, 1);
         
-        $text = "$it->what ($it->where) \\n " ;
+        $text = "$it->what ($it->where) \\n" ;
         foreach ($it->stats as $v) {
                 $text .= $v->vote . ": " . $v->count . "\\n";
         }
         $text .= "Details unter http://training.munich-rugbears.de/" . $it->tid . "\\n";
         $text .=   "\n";
         
-        $item->addAttendee("rugbyball@munich-rugbears.de", "PARTSTAT=ACCEPTED;CN=\"Rugby Ball\"");
-        $item->addAttendee("john@somewhere.de", "PARTSTAT=DECLINED;CN=\"John Doe\"");
-        
+        foreach (Trainings::votes($it->tid) as $v) {
+            $item->addAttendee($v->name ."@munich-rugbears.de","PARTSTAT=" . $pstate[$v->vid]. ";CN=\"$v->name\"");
+    //        $item->addAttendee("rugbyball@munich-rugbears.de", "PARTSTAT=ACCEPTED;CN=\"Rugby Ball\"");
+    //        $item->addAttendee("john@somewhere.de", "PARTSTAT=DECLINED;CN=\"John Doe\"");
+        }
         
         
         $item->setLongDescription($text);
