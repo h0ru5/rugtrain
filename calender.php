50e6990ce82727158f67f2a5247e9ba42de1e74c
h0ru5 <johannes.hund@gmail.com>
2013-04-21 17:07:47 +0200

    added link Signed-off-by:h0ru5 <johannes.hund@gmail.com>

--- calender.php
+++ calender.php
@@ -11,10 +11,18 @@ function addEvents($cal) {
         $item->setLocation($it->where);
         $item->setShortDescription($it->what);
         $dat = new DateTime($it->when);
-        $item->setStart($dat->format("Y"),$dat->format("m"),$dat->format("d"));
-        $dat->modify("+1 day");
-        $item->setEnd($dat->format("Y"),$dat->format("m"),$dat->format("d"));
+        $item->setStart($dat->format("Y"),$dat->format("m"),$dat->format("d"), 0, 1, "", 1);
+        $dat->modify("+1 day"); //refractor dates to start and end
+        $item->setEnd($dat->format("Y"),$dat->format("m"),$dat->format("d"),0, 1, "", 1);
         $item->setDuration(0, 1);
+        
+        $text = "$it->what ($it->where) \\n " ;
+        $text .= "Details unter http://training.munich-rugbears.de/" . $it->tid;
+        $text .=   "\n";
+        
+        $item->setLongDescription($text);
+        $item->setURL("http://training.munich-rugbears.de/" . $it->tid);
+        
         $cal->add($item);
     }
 }
