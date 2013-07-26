b8ff3d0015cc474c908eaad4afb0ccefbc9a6938
h0ru5 <johannes.hund@gmail.com>
2013-07-26 22:18:29 +0200

    updated database schema Signed-off-by:h0ru5 <johannes.hund@gmail.com>

--- database.sql
+++ database.sql
@@ -1,24 +1,20 @@
 -- phpMyAdmin SQL Dump
--- version 3.4.7
+-- version 4.0.4
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 18, 2012 at 12:08 AM
--- Server version: 5.5.28
--- PHP Version: 5.2.17-0.dotdeb.0
+-- Generation Time: Jul 26, 2013 at 08:17 PM
+-- Server version: 5.5.28-1~dotdeb.0
+-- PHP Version: 5.4.16-1~alfa.0
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `usr_web617_1`
 --
+CREATE DATABASE IF NOT EXISTS `usr_web617_1` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `usr_web617_1`;
 
 -- --------------------------------------------------------
 
@@ -29,13 +25,13 @@ SET time_zone = "+00:00";
 CREATE TABLE IF NOT EXISTS `kommentare` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `tid` int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'references trainings table',
-  `autor` varchar(255) COLLATE latin1_german1_ci NOT NULL DEFAULT '',
-  `msg` text COLLATE latin1_german1_ci NOT NULL,
+  `autor` varchar(255) CHARACTER SET latin1 COLLATE latin1_german1_ci NOT NULL DEFAULT '',
+  `msg` text CHARACTER SET latin1 COLLATE latin1_german1_ci NOT NULL,
   `time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`id`,`tid`),
   KEY `time` (`time`),
   KEY `tid` (`tid`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -48,7 +44,21 @@ CREATE TABLE IF NOT EXISTS `mailliste` (
   `name` text COLLATE latin1_german1_ci NOT NULL,
   `email` text COLLATE latin1_german1_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci AUTO_INCREMENT=42 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shouts`
+--
+
+CREATE TABLE IF NOT EXISTS `shouts` (
+  `id` int(32) unsigned NOT NULL AUTO_INCREMENT,
+  `autor` varchar(512) NOT NULL,
+  `msg` text NOT NULL,
+  `zeit` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -58,7 +68,7 @@ CREATE TABLE IF NOT EXISTS `mailliste` (
 
 CREATE TABLE IF NOT EXISTS `t2` (
   `tid` int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'references trainings table',
-  `Name` varchar(255) COLLATE latin1_german1_ci NOT NULL DEFAULT 'unbekannt',
+  `Name` varchar(255) CHARACTER SET utf8 NOT NULL DEFAULT 'unbekannt',
   `Vote` tinyint(4) NOT NULL DEFAULT '0',
   `time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`tid`,`Name`),
@@ -75,10 +85,13 @@ CREATE TABLE IF NOT EXISTS `t2` (
 CREATE TABLE IF NOT EXISTS `trainings` (
   `tid` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `when` datetime NOT NULL,
-  `what` varchar(1024) COLLATE latin1_german1_ci NOT NULL,
-  `where` varchar(1024) COLLATE latin1_german1_ci NOT NULL,
+  `what` varchar(1024) NOT NULL,
+  `where` varchar(1024) NOT NULL,
+  `start` datetime DEFAULT NULL,
+  `end` datetime DEFAULT NULL,
+  `type` enum('TRAINING','OTHER') NOT NULL,
   PRIMARY KEY (`tid`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -90,18 +103,7 @@ CREATE TABLE IF NOT EXISTS `votes` (
   `id` tinyint(4) NOT NULL AUTO_INCREMENT,
   `text` varchar(64) COLLATE latin1_german1_ci NOT NULL DEFAULT '',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci AUTO_INCREMENT=4 ;
-
--- Table for shoutbox
-CREATE TABLE IF NOT EXISTS `shouts` (
-  `id` int(32) unsigned NOT NULL AUTO_INCREMENT,
-  `autor` varchar(512) NOT NULL,
-  `msg` text NOT NULL,
-  `zeit` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
-
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci;
 
 --
 -- Constraints for dumped tables
@@ -118,7 +120,3 @@ ALTER TABLE `kommentare`
 --
 ALTER TABLE `t2`
   ADD CONSTRAINT `t2_ibfk_2` FOREIGN KEY (`tid`) REFERENCES `trainings` (`tid`) ON DELETE CASCADE ON UPDATE CASCADE;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
