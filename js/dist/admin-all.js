ac64899ec6335f53a9259842f35911680871d444
h0ru5 <johannes.hund@gmail.com>
2013-08-24 23:12:37 +0200

    fixed date-time-picker Signed-off-by:h0ru5 <johannes.hund@gmail.com>

--- js/dist/admin-all.js
+++ js/dist/admin-all.js
@@ -14235,18 +14235,32 @@
 	$.timepicker.setDefaults($.timepicker.regional['de']);
 })(jQuery);
 
-angular.module('dateTimePicker', []).directive("dateTimePicker", ['$compile', function($compile) {
-        return function(scope, element, attrs) {
-             dtpOptions = {};
-              if(attrs.timeFormat.length>0) {
+angular.module('dateTimePicker', []).directive("dateTimePicker", ['$compile','$log', function($compile,$log) {
+        return {
+            require: '?ngModel',
+            link: function(scope, element, attrs, ngModel) {
+             dtpOptions = {
+                 onSelect: function(selectedDateTime) {
+                     $log.log("set to " + element.val());
+                     scope.$apply(updated());
+                 }
+             };
+             function updated() {
+                 ngModel.$setViewValue(element.val());
+             }
+             
+              if(attrs.timeFormat && attrs.timeFormat.length>0) {
                 dtpOptions.timeFormat = attrs.timeFormat
                 }
-                if(attrs.dateFormat.length>0) {
+                if(attrs.dateFormat && attrs.dateFormat.length>0) {
                 dtpOptions.dateFormat = attrs.dateFormat
                 }
+
             element.datetimepicker(dtpOptions);
+            updated();
         }
-}]);
+        }
+}]); 
 angular.module('myTable', []).directive('myTable', function() {
         return function(scope, element, attrs) {
 
--- js/ng-directives/dateTimePicker.js
+++ js/ng-directives/dateTimePicker.js
@@ -1,12 +1,26 @@
-angular.module('dateTimePicker', []).directive("dateTimePicker", ['$compile', function($compile) {
-        return function(scope, element, attrs) {
-             dtpOptions = {};
+angular.module('dateTimePicker', []).directive("dateTimePicker", ['$compile','$log', function($compile,$log) {
+        return {
+            require: '?ngModel',
+            link: function(scope, element, attrs, ngModel) {
+             dtpOptions = {
+                 onSelect: function(selectedDateTime) {
+                     $log.log("set to " + element.val());
+                     scope.$apply(updated());
+                 }
+             };
+             function updated() {
+                 ngModel.$setViewValue(element.val());
+             }
+             
               if(attrs.timeFormat && attrs.timeFormat.length>0) {
                 dtpOptions.timeFormat = attrs.timeFormat
                 }
                 if(attrs.dateFormat && attrs.dateFormat.length>0) {
                 dtpOptions.dateFormat = attrs.dateFormat
                 }
+
             element.datetimepicker(dtpOptions);
+            updated();
+        }
         }
 }]); 
